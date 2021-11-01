44d6d7cd4994d9e75d78468adcc4d730ed954eb9
nakaa_0913 <nakaakuk0913@gmail.com>
2021-11-01 19:47:22 +0900

    stagedata.hの追加

---
+++ stagedata.h
@@ -0,0 +1,14 @@
+//#include "main.h"
+//
+//#define MAX_STAGE (30)			//最大ステージ
+//
+//typedef struct {
+//	int stagenum;				//ステージ番号
+//	int mission1;				//ミッションは何か
+//	int mission2;
+//	int mission3;
+//	bool clear1;				//ミッションをクリアしたか
+//	bool clear2;
+//	bool clear3;
+//	float cleartime;			//クリアタイム
+//}STAGEDATA;
